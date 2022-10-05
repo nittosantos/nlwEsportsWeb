da1bf42783f5bde0f9ec3ea744caf13fb81c3898
Nitto <nitto_kun@hotmail.com>
2022-10-04 22:48:11 -0300

    feat: :sparkles: finalizando o component visual

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,81 @@
-import './styles/main.css'
+import './styles/main.css';
+import logoImg from './assets/logo.svg';
+import { MagnifyingGlassPlus } from 'phosphor-react';
 
 function App() {
   return (
-    <div className="w-8 h-8 bg-violet-800">
-      Hello
+    <div className="max-w-[1344px] mx-auto flex items-center flex-col my-20">
+      <img src={logoImg} alt="Logo NLW Esports" />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{' '}
+        <span className="text-transparent bg-clip-text bg-nlw-gradient">
+          duo
+        </span>{' '}
+        está aqui
+      </h1>
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game1.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anuncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game2.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anuncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game3.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anuncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game4.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anuncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game5.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anuncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game6.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anuncios</span>
+          </div>
+        </a>
+      </div>
+
+      <div className="pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
+        <div className="bg-[#2a2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black">
+              Não encontrou seu duo?
+            </strong>
+            <span className="text-zinc-400 block">
+              Publique um anúncio para encontrar novos players!
+            </span>
+          </div>
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-700 rounded text-white flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
