48a214a55b1c7d7fa9686e87e208e94e9d400fa2
Nitto <nitto_kun@hotmail.com>
2022-10-04 22:47:55 -0300

    chore: :memo: apenas modificando algumas configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,19 @@
 module.exports = {
   content: [
     './src/**/*.tsx',
+    './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['inter', 'sans-serif']
+      },
+      backgroundImage: {
+        galaxy: "url('/bg-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572fc 10.33%, #43e7ad 40.33%, #e1d55d 95.34%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
+      }
+    },
   },
   plugins: [],
 }
