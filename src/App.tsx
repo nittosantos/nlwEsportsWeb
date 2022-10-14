7fdb931cf02dc8afb1a1eb6142de7baed5def16e
Nitto <nitto_kun@hotmail.com>
2022-10-13 21:21:34 -0300

    style: :zap: finalizando a casca do modal

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ function App() {
         <CreateAddBanner />
         <Dialog.Portal>
           <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
-          <Dialog.Content className="fixed bg-[#2a2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px]">
+          <Dialog.Content className="fixed bg-[#2a2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
             <Dialog.Title>Publique um anúncio</Dialog.Title>
             <Dialog.Content>Bem vindo mundo</Dialog.Content>
           </Dialog.Content>
